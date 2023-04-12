68f7851ec333444c7148ea86c3401349576ffc2e
M3rtButur4K <mertilein@web.de>
2023-04-12 23:57:02 +0200

    More Stuff
    - Universal Render Pipeline added
    - TextMeshPro package added
    - Static Shaders created
    - Main Menu & Level scene edited

---
+++ Assets/Scripts/Functions/OfficeManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This Manages the functions for the Office - M3rt
+/// </summary>
+public class OfficeManager : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
